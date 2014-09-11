9d649019645da610f6100abd96280468373caf6c
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-09-10 22:45:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SemoApp::Application.config.secret_key_base = '6a3bb477ac898c70b74236252deb5451c52d138a8eb3c92240a3fbd465cfbabdeabca3298518ff9a96a6cc52f7d427c0e5a34a453f4d9f098a841805462aa3e0'
